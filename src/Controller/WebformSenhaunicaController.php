4e65b88e03566b429cc8aec3c2f8dbd1e5c5fbc2
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2025-09-09 16:10:06 -0300

    integrando senha única

---
+++ src/Controller/WebformSenhaunicaController.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\webform_senhaunica\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Returns responses for Webform Senha Única routes.
+ */
+final class WebformSenhaunicaController extends ControllerBase {
+
+  /**
+   * Builds the response.
+   */
+  public function __invoke(): array {
+
+    die('To aqui galera');
+
+    $user = Uspdev\Senhaunica\Senhaunica::getUserDetail();
+
+    $data = $webform_submission->getData();
+    
+    $data['wsuserid'] = 12345;
+    $data['loginUsuario'] = 'usuario_teste';
+    $data['nomeUsuario'] = 'Nome de Exemplo';
+
+    $webform_submission->setData($data);
+    $webform_submission->save();
+
+    $build['content'] = [
+      '#type' => 'item',
+      '#markup' => $this->t('It works!'),
+    ];
+
+    return $build;
+  }
+
+}
